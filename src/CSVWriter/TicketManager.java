ccf702f114b99e9d38dfb35b5b908be739f20192
Chen Zhi Wei <chinzhiweiblank@gmail.com>
2019-11-16 23:35:46 +0800

    CSVWriter Docs

--- src/CSVWriter/TicketManager.java
+++ src/CSVWriter/TicketManager.java
@@ -3,10 +3,26 @@ package CSVWriter;
 import java.io.IOException;
 
 /**
- * ticket manager interface for others ticket managers to implement
+ * Ticket manager interface for others ticket managers to implement
  */
 public interface TicketManager {
+	/**
+	 * Reads the price
+	 * @param movieClass Class of Movie booked
+	 * @param age Age of Person booking
+	 * @param day Day when Ticket was booked
+	 * @return Ticket Price
+	 * @throws IOException if input is wrong
+	 */
 	public double readPrice(int movieClass, int age, int day) throws IOException;
 
+	/**
+	 * Reads the price
+	 * @param movieClass Class of Movie booked
+	 * @param age Age of Person booking
+	 * @param day Day when Ticket was booked
+	 * @return Ticket Price
+	 * @throws IOException if input is wrong
+	 */
 	public int updatePrice(int movieClass, int age, int day, int price) throws IOException;
 }
